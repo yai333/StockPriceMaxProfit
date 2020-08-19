c1821d7b1872f489f9192b33fe5d2f12431a3260
YI AI <aiyi.hack@gmail.com>
2020-08-19 20:41:49 +1000

    fix typo

--- main.test.py
+++ main.test.py
@@ -21,7 +21,7 @@ class TestStockPricesMaxProfit(unittest.TestCase):
         self.assertEqual(max_sell, 11)
         self.assertEqual(max_profit, 6)
 
-    def test_with_duplicate_values(self):
+    def test_with_duplicated_values(self):
         stock_prices_yesterday = [10, 7, 1, 1, 20, 10]
         min_buy, max_sell, max_profit = get_max_profit(stock_prices_yesterday)
 
